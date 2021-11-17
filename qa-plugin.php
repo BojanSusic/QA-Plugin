4afce080a05123dfb330da14bfbdbff18225616b
BojanSusic <60573368+BojanSusic@users.noreply.github.com>
2021-11-17 12:29:43 +0100

    Vesion 1.0.3

--- qa-plugin.php
+++ qa-plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: QA Plugin
  * Plugin URI: https://singularity.is
  * Description: This is a qa plugin with checklist for object web pages
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: Miljana Pinic, Bojan Susic, Djordje Avram
  **/
 
