fcfc51324b4b4213ba1115963279500e4f921b07
BojanSusic <60573368+BojanSusic@users.noreply.github.com>
2021-11-17 12:18:53 +0100

    QA plugin first vesion

---
+++ requests-array.php
@@ -0,0 +1,135 @@
+<?php
+
+   $requests_array =  [
+        [
+            'checked' => 0,
+            'title' => 'Favicon set',
+            'description' => 'On frontend and Wp-admin also',
+        ],
+       [
+           'checked' => 0,
+           'title' => 'Meta tags set',
+           'description' => 'Title and meta description',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Cookie banner exist',
+           'description' => '',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Default page styled (404)',
+           'description' => 'Check that 404 is not on pages that we used (home, wohnen...)',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Everything is translated on the proper language',
+           'description' => 'There is no dummy pages, contact form translated...',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Images are not pixelated',
+           'description' => '',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'All links are correctly linked',
+           'description' => '',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Recaptcha works',
+           'description' => '',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Page looks good on mobile',
+           'description' => 'Check that everything is scaled, there is no overlapping...',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Check Datenschutzerklaerung page',
+           'description' => 'Check that plugins that we used are on list.',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Website have Rechtliche Informationen',
+           'description' => '',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Search engine disabled on DEV',
+           'description' => '',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Search engine enabled od PROD',
+           'description' => '',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Flats are sorted',
+           'description' => '',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Check pageload speed',
+           'description' => '',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Google Analytics connected',
+           'description' => '',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'API connected',
+           'description' => '',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Bewerben button works',
+           'description' => 'There is link to exact reference number on wohnen and to general form on gewerbe',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Map correctly applied',
+           'description' => 'Correct address and language',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Correct address on page',
+           'description' => 'Startsite, footer, map...',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Contact form works',
+           'description' => '',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Correct error message on contact form',
+           'description' => '',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Redirect after contact form works',
+           'description' => '',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Wincasa alarm works',
+           'description' => 'Subscribe, redirect, error, e-mail',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'Clear cache',
+           'description' => 'Check if page works correctly in incognito mode',
+       ],
+       [
+           'checked' => 0,
+           'title' => 'There is no old data in settings',
+           'description' => '',
+       ],
+
+    ];
